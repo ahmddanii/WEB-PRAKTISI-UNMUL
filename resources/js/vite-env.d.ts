fd078d979025609d0ae41e5e8313dccab195ef90
Ahmad Dani <ad480132@gmail.com>
2026-06-25 20:29:28 +0800

    feat: setup inertia.js and typescript configurations

---
+++ resources/js/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+import { Page } from '@inertiajs/core';
+import { ReactElement, ReactNode } from 'react';
+
+declare global {
+  interface Window {
+    // Custom window properties can go here
+  }
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html class="light scroll-smooth scroll-pt-16" lang="id">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
+    
+    <!-- Fonts & Icons -->
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;700&family=Jetbrains+Mono:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet">
+
+    <!-- Scripts & Styles -->
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+    @inertiaHead
+</head>
+<body class="bg-background text-on-background">
+    @inertia
+</body>
+</html>
