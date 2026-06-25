76d8032eea8774993bb0df1fcd48e0291327a2bc
Ahmad Dani <ad480132@gmail.com>
2026-06-25 20:33:40 +0800

    feat: add default admin user and run related seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        // Seeding default Admin
+        User::create([
+            'name' => 'Admin Praktisi',
+            'username' => 'admin',
+            'email' => 'admin@praktisi.unmul.ac.id',
+            'password' => Hash::make('password'),
+        ]);
+
+        // Call other seeders
+        $this->call([
+            MataKuliahSeeder::class,
+            RuanganSeeder::class,
+            KelasPraktikumSeeder::class,
+        ]);
     }
 }
